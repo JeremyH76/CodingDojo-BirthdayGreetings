bbe1a78572031e76b996f0320e1b50af93ef83c4
Jérémy Hure <hure.j@sfeir.com>
2022-10-06 11:20:14 +0200

    More tests

--- TestProject/UnitTest.cs
+++ TestProject/UnitTest.cs
@@ -31,5 +31,57 @@ namespace TestProject
             bg.FindSearchAndSend(new DateOnly(2022, 5, 3));
             senderMock.Verify(x => x.SendGreetings(It.Is<List<Friend>>(x => x.Count == 1 && friends[0].Equals(x[0]))), Times.Once);
         }
+
+        [Fact]
+        public void TestNoBirthdayInLotOfFriends()
+        {
+            List<Friend> friends = new List<Friend>() {
+                new Friend("a", "b", new DateOnly(1960, 5, 3), "a.b@gmail.com"),
+                new Friend("g", "a", new DateOnly(1972, 2, 20), "g.a@gmail.com"),
+                new Friend("z", "r", new DateOnly(1961, 12, 9), "z.r1212@gmail.com")
+            };
+            Mock<IFriendDao> friendDao = new Mock<IFriendDao>();
+            friendDao.Setup(x => x.GetFriends()).Returns(friends);
+            Mock<ISendGreetings> senderMock = new Mock<ISendGreetings>();
+            senderMock.Setup(x => x.SendGreetings(It.IsAny<List<Friend>>()));
+            BirthdayGreetings bg = new BirthdayGreetings(friendDao.Object, senderMock.Object);
+            bg.FindSearchAndSend(new DateOnly(2022, 5, 4));
+            senderMock.Verify(x => x.SendGreetings(It.IsAny<List<Friend>>()), Times.Never);
+        }
+
+        [Fact]
+        public void TestBirthdayInLotOfFriends()
+        {
+            List<Friend> friends = new List<Friend>() {
+                new Friend("a", "b", new DateOnly(1960, 5, 3), "a.b@gmail.com"),
+                new Friend("g", "a", new DateOnly(1972, 2, 20), "g.a@gmail.com"),
+                new Friend("z", "r", new DateOnly(1961, 12, 9), "z.r1212@gmail.com")
+            };
+            Mock<IFriendDao> friendDao = new Mock<IFriendDao>();
+            friendDao.Setup(x => x.GetFriends()).Returns(friends);
+            Mock<ISendGreetings> senderMock = new Mock<ISendGreetings>();
+            senderMock.Setup(x => x.SendGreetings(It.IsAny<List<Friend>>()));
+            BirthdayGreetings bg = new BirthdayGreetings(friendDao.Object, senderMock.Object);
+            bg.FindSearchAndSend(new DateOnly(2022, 2, 20));
+            senderMock.Verify(x => x.SendGreetings(It.Is<List<Friend>>(x => x.Count == 1 && friends[1].Equals(x[0]))), Times.Once);
+        }
+
+        [Fact]
+        public void TestTwoBirthdayToday()
+        {
+            List<Friend> friends = new List<Friend>() {
+                new Friend("a", "b", new DateOnly(1960, 5, 3), "a.b@gmail.com"),
+                new Friend("g", "a", new DateOnly(1972, 2, 20), "g.a@gmail.com"),
+                new Friend("z", "r", new DateOnly(1961, 12, 9), "z.r1212@gmail.com"),
+                new Friend("m", "j", new DateOnly(1963, 2, 20), "m.j@gmail.com")
+            };
+            Mock<IFriendDao> friendDao = new Mock<IFriendDao>();
+            friendDao.Setup(x => x.GetFriends()).Returns(friends);
+            Mock<ISendGreetings> senderMock = new Mock<ISendGreetings>();
+            senderMock.Setup(x => x.SendGreetings(It.IsAny<List<Friend>>()));
+            BirthdayGreetings bg = new BirthdayGreetings(friendDao.Object, senderMock.Object);
+            bg.FindSearchAndSend(new DateOnly(2022, 2, 20));
+            senderMock.Verify(x => x.SendGreetings(It.Is<List<Friend>>(x => x.Count == 2 && friends[1].Equals(x[0]) && friends[3].Equals(x[1]))), Times.Once);
+        }
     }
 }
